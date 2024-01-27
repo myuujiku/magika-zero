681c89bf30a5df1a68c48e7629f8da1cdccb1071
myuujiku <myujiku@proton.me>
2024-01-27 15:24:21 +0100

    initial commit

---
+++ src/main/java/myujiku/magikazero/MagikaZERODataGenerator.java
@@ -0,0 +1,11 @@
+package myujiku.magikazero;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class MagikaZERODataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
