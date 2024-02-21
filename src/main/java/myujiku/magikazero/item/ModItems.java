982cc9d32b5345422f34d20bb512fcd09bd73bd4
myuujiku <myujiku@proton.me>
2024-02-21 15:39:06 +0100

    add method for adding items to item groups

--- src/main/java/myujiku/magikazero/item/ModItems.java
+++ src/main/java/myujiku/magikazero/item/ModItems.java
@@ -1,9 +1,13 @@
 package myujiku.magikazero.item;
 
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.ItemGroups;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
+import net.minecraft.registry.RegistryKey;
 import net.minecraft.util.Identifier;
 
 public class ModItems {
@@ -15,5 +19,12 @@ public class ModItems {
 
     private static void registerItem(Item item, String path) {
         Registry.register(Registries.ITEM, new Identifier("magika-zero", path), item);
+    }
+
+    private static void registerItem(Item item, String path, RegistryKey<ItemGroup> group) {
+        registerItem(item, path);
+        ItemGroupEvents.modifyEntriesEvent(group).register(content -> {
+                content.add(item);
+        });
     }
 }
