77eb2dd1d959b372c8998243e445f3f076f4e4fe
myuujiku <myujiku@proton.me>
2024-02-21 15:40:06 +0100

    make Book of Knowledge open the test UI

--- src/main/java/myujiku/magikazero/item/BookOfKnowledge.java
+++ src/main/java/myujiku/magikazero/item/BookOfKnowledge.java
@@ -1,8 +1,29 @@
 package myujiku.magikazero.item;
 
+import myujiku.magikazero.MagikaZERO;
+import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.server.network.ServerPlayerEntity;
+import net.minecraft.util.Hand;
+import net.minecraft.util.TypedActionResult;
+import net.minecraft.world.World;
 
 public class BookOfKnowledge extends Item {
+    public record OpenUIPacket() {}
+
+    @Override
+    public TypedActionResult<ItemStack> use(World world, PlayerEntity user, Hand hand) {
+        if (world.isClient) {
+            //MagikaZERO.CHANNEL.clientHandle().send(new Packet());
+            return super.use(world, user, hand);
+        }
+
+        MagikaZERO.CHANNEL.serverHandle((ServerPlayerEntity) user).send(new OpenUIPacket());
+
+        return TypedActionResult.pass(user.getStackInHand(hand));
+    }
+
     public BookOfKnowledge(Settings settings) {
         super(settings);
     }
