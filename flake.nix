6756572c9138e1ff572d13baf637fdc48c86b5ff
myuujiku <myujiku@proton.me>
2024-02-29 00:48:31 +0100

    make necessary dependencies available on NixOS with `nix develop`

---
+++ flake.nix
@@ -0,0 +1,24 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs }:
+  
+  let 
+    pkgs = nixpkgs.legacyPackages.x86_64-linux;
+    libs = with pkgs; [
+        libpulseaudio
+        libGL
+        glfw
+        openal
+        stdenv.cc.cc.lib
+      ];
+  in {
+    devShell.x86_64-linux = pkgs.mkShell {
+      packages = [];
+      buildInputs = libs;
+      LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath libs;
+    };
+  };
+}
